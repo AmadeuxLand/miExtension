9f266dd0878c74a9e2aaf6448e50e80a085328a6
AmadeuxLand <104588224+AmadeuxLand@users.noreply.github.com>
2022-05-01 19:33:59 -0500

    Update miExtension.js

--- miExtension.js
+++ miExtension.js
@@ -38,7 +38,7 @@ class MyExtension {
     // You can just return a value: any string, boolean, or number will work.
     // If you have to perform an asynchronous action like a request, just return a Promise.
     // The block will wait until the Promise resolves and return the resolved value.
-    return 'Hello, world!';
+    return 'Hola munditoooo!';
   }
 }
 
